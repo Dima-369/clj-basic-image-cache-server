6e4d68a5599796e316f703a441a9070e3e14140e
Dmytro Butemann <dmytro.butemann@gmail.com>
2016-12-29 14:15:42 +0100

    Add description and url to project.clj

--- project.clj
+++ project.clj
@@ -1,16 +1,17 @@
 (defproject image-server "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Simple server which caches image requests"
+  :url "https://github.com/Gira-X/clj-basic-image-cache-server"
   :min-lein-version "2.0.0"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "MIT License"
+	    :url "http://www.opensource.org/licenses/mit-license.php"
+	    :distribution :repo}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [compojure "1.5.1"]
-                 [ring/ring-defaults "0.2.1"]
-                 [http-kit "2.2.0"]
-                 [digest "1.4.5"]
-                 [base64-clj "0.1.1"]
-                 [com.taoensso/timbre "4.8.0"]
-                 [commons-validator "1.5.1"]]
+		 [compojure "1.5.1"]
+		 [ring/ring-defaults "0.2.1"]
+		 [http-kit "2.2.0"]
+		 [digest "1.4.5"]
+		 [base64-clj "0.1.1"]
+		 [com.taoensso/timbre "4.8.0"]
+		 [commons-validator "1.5.1"]]
   :main ^:skip-aot image-server.core
   :profiles {:uberjar {:aot :all}})
