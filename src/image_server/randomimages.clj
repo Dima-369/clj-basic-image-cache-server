5ef0c5abd6a8744de7d32e341f8c3a7372883305
Dmytro Butemann <dmytro.butemann@gmail.com>
2016-12-29 22:26:10 +0100

    Access random files from inside the jar

--- src/image_server/randomimages.clj
+++ src/image_server/randomimages.clj
@@ -1,13 +1,15 @@
 (ns image-server.randomimages
   (:require [image-server.utils :refer :all]
+            [clojure.java.io :refer [file resource]]
             [org.httpkit.server :refer [with-channel]])
   (:gen-class))
 
-(def random-picture-dir "resources/random/")
+(def random-picture-dir "random/")
 (def random-picture-amount 25)
 
 (defn get-random-pic []
-  (str random-picture-dir (inc (rand-int random-picture-amount)) ".jpg"))
+  (file (resource (str random-picture-dir (inc (rand-int random-picture-amount))
+                 ".jpg"))))
 
 (defn get-random [req]
   (with-channel req channel
