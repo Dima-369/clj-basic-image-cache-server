74da0d93ea737ac767d5617a9b31ea4a27443973
Dmytro Butemann <dmytro.butemann@gmail.com>
2016-12-30 20:54:23 +0100

    Add test for the race condition download issue
    See #2

--- test/image_server/core_test.clj
+++ test/image_server/core_test.clj
@@ -76,15 +76,29 @@
 (defn count-downloads-in-log [s log]
   (count (re-seq (re-pattern (str "Downloaded \"" s "\"")) log)))
 
+(def race-condition-test-amount 8)
+
+(defn single-concurrent-test []
+  (delete-files [(str cache-directory (sha-256 large-test-image))])
+  (doseq [f (doall (repeatedly 2 #(future (is-redirect? large-test-image))))]
+    @f)
+  ; required for the filesystem to catch up
+  (Thread/sleep 100))
+
+; See issue #2
+(defn test-concurrent-race-condition
+  "Tests multiple concurrent calls to the same URL several times, hoping to
+  trigger the race condition where the image is downloaded multiple times"
+  []
+  (doall (repeatedly race-condition-test-amount single-concurrent-test)))
+
 (deftest server
-  (delete-files [log-file
-                 (str cache-directory (sha-256 test-image))
-                 (str cache-directory (sha-256 large-test-image))])
+  (delete-files [log-file])
   (let [httpkitserver (-main)]
     (let [{:keys [status body]} (localhost)]
       (is (= status 404))
       (is (= body "Page not found")))
-    (is (are-random-pics-random? 30))
+    (is (are-random-pics-random? (/ random-picture-amount 2)))
     (let [{:keys [status body]} (localhost "get/invalidbase64")]
       (is (= status 400))
       (is (= body "Invalid base64")))
@@ -94,14 +108,15 @@
       (is (= body "Decoded URL is not valid")))
     ; second call returns the cached image which should be smaller than the
     ; original image
+    (delete-files [(str cache-directory (sha-256 test-image))])
     (is (> (get-content-length test-image #(= test-image %) {:delay 0})
            (get-content-length test-image is-local-url? {:delay 1000})))
-    ; testing multiple concurrent calls
-    (doseq [f (doall (repeatedly 15 #(future (is-redirect? large-test-image))))]
-      @f)
-    ; wait until all server threads are finished, the above future calls are
-    ; usually faster
+    (test-concurrent-race-condition)
+    ; wait until all server threads are finished, the future calls from
+    ; test-concurrent-race-condition are usually faster
     (Thread/sleep 1000)
     (httpkitserver))
   (let [log (slurp log-file)]
-    (is (= 1 (count-downloads-in-log large-test-image log)))))
+    ; TODO: Check if any exceptions are logged
+    (is (= race-condition-test-amount
+           (count-downloads-in-log large-test-image log)))))
